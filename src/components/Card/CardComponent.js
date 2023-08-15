abc79a12133a9f7b131f83f4489f5a74c5dffcf5
radenkosetka7 <58879645+radenkosetka7@users.noreply.github.com>
2023-08-15 18:29:04 +0200

    image

--- src/components/Card/CardComponent.js
+++ src/components/Card/CardComponent.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Card } from 'antd';
 import './Card.css';
-import Logo from '../../assets/web-shop-logo(1).png'
 const { Meta } = Card;
-const CardComponent = () => (
-    <Card
-        hoverable
-        className='card'
-        cover={<img alt="example" className='image' src={Logo} />}
-    >
-        <Meta className='price' title="Europe Street beat" description="17.00 BAM" />
-    </Card>
-);
+const CardComponent = () => {
+
+    //poslace se ime slike a podrazumijevana putanja ce biti asssets/procuts+imeSlike
+    const image = require('../../assets/snoweror.jpg')
+
+    return(
+        <Card
+            hoverable
+            className='card'
+            cover={<img alt="example" className='image' src={image}/>}
+        >
+            <Meta className='price' title="Europe Street beat" description="17.00 BAM"/>
+        </Card>
+    );
+}
 export default CardComponent;
